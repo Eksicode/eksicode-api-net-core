62e42468dcef6581cb2d7e8730469cd0b102a055
cagan <ibrahimcagan@gmail.com>
2020-07-12 13:50:17 +0300

    Created solution

---
+++ EksiCode/EksiCode.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EksiCode.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly ILogger<AuthenticationController> _logger;
+        public AuthenticationController(ILogger<AuthenticationController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
